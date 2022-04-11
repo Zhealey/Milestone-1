0ca00b877977bc97cb4db02ce2240acbce3364bc
ZHealey <zackhealey@yahoo.com>
2022-04-11 18:10:42 -0400

    fixed code and now works properly, added files for after quiz finished

--- app.js
+++ app.js
@@ -1,152 +1,158 @@
 const question = document.querySelector('#question');
-const choices = Array.from(document.querySelector('.choice-text'));
+const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
-const scoreTest = document.querySelector('#score');
+const scoreText = document.querySelector('#score');
 
 let currentQuestion = {}
-let acceptingAnswers = true
+let acceptingAnswer = true
 let score = 0
 let questionCounter = 0
 let availableQuestions = []
 
 let questions = [
     {
-        question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
+        question: 'What was the first video game?',
+        choice1: 'Tetris',
+        choice2: "Asteroids",
+        choice3: "Pong",
+        choice4: "Donkey Kong",
+        answer: 3,
+    },
+    {
+        question: "Who saves the Mushroom Kingdom?",
+        choice1: "Link",
+        choice2: "Bowser",
+        choice3: "Samus",
+        choice4: "Mario",
+        answer: 4,
+    },
+    {
+        question: "What is MegaMans Original Name?",
+        choice1: "MetalMan",
+        choice2: "RockMan",
+        choice3: "RobotMan",
+        choice4: "ToyMan",
         answer: 2,
     },
     {
-        question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '3',
-        choice3: '21',
-        choice4: '17',
+        question: "Who is the Princess in Legend of Zelda",
+        choice1: "Zelda",
+        choice2: "Daisy",
+        choice3: "Link",
+        choice4: "Peach",
+        answer: 1,
+    },
+    {
+        question: "What company created Resident Evil?",
+        choice1: "Capcom",
+        choice2: "Nintendo",
+        choice3: "Ubisoft",
+        choice4: "Atari",
+        answer: 1,
+    },
+    {
+        question: "Which of these games does not let you fish?",
+        choice1: "Sonic Adventure",
+        choice2: "Legend of Zelda: Ocarina of Time",
+        choice3: "Pokemon Blue",
+        choice4: "Mario Kart 64",
+        answer: 4,
+    },
+    {
+        question: "Which of these is not a real game?",
+        choice1: "Katamari Damacy",
+        choice2: "Jiggy's Adventure Palace",
+        choice3: "Perfect Dark",
+        choice4: "Kid Klown in Crazy Chase",
         answer: 2,
     },
     {
-        question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
+        question: "What does Kazooie shoot in Banjo-Tooie?",
+        choice1: 'Eggs',
+        choice2: 'Bullets',
+        choice3: 'Fire',
+        choice4: 'Bees',
+        answer: 1,
     },
     {
-        question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
+        question: "Who is the main villian in Mega Man",
+        choice1: "Dr. Light",
+        choice2: "Dr. Oz",
+        choice3: "Dr. Eggman",
+        choice4: "Dr. Wily",
+        answer: 4,
     },
     {
-        question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
-    },
-    {
-        question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
-    },
-    {
-        question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
-    },
-    {
-        question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
-    },
-    {
-        question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
-    },
-    {
-        question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
+        question: "Which Nintendo Character can copy the ability of anyone they eat?",
+        choice1: "Wario",
+        choice2: "Yoshi",
+        choice3: "Bowser",
+        choice4: "Kirby",
+        answer: 4,
     },
 ]
 
-const SCORE_POINTS = 1
-const MAX_QUESTIONS = 4
+const POINTS_SCORED = 1
+const TOTAL_QUESTIONS = 10
 
 startGame = () => {
     questionCounter = 0
-    score =0
+    score = 0
     availableQuestions = [...questions]
     getNewQuestion()
 }
 
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionsCounter > MAX_QUESTIONS) {
+    if(availableQuestions.length === 0 || questionCounter > TOTAL_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assignn('/end.html')
+        return window.location.assign('/.html')
     }
 
     questionCounter++
-    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-
+    progressText.innerText = `Question ${questionCounter} of ${TOTAL_QUESTIONS}`
+    
     const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
     currentQuestion = availableQuestions[questionsIndex]
     question.innerText = currentQuestion.question
 
     choices.forEach(choice => {
-        const numer = choice.dataset['number']
+        const number = choice.dataset['number']
         choice.innerText = currentQuestion['choice' + number]
     })
 
     availableQuestions.splice(questionsIndex, 1)
 
-    acceptingAnswers = true
+    acceptingAnswer = true
 }
 
 choices.forEach(choice => {
     choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return
+        if(!acceptingAnswer) return
 
-        acceptingAnswers = false
+        acceptingAnswer = false
         const selectedChoice = e.target
         const selectedAnswer = selectedChoice.dataset['number']
 
         let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
 
         if(classToApply === 'correct') {
-            incrementScore(SCORE_POINTS)
+            addScore(POINTS_SCORED)
         }
 
-        selectedChoice.parentElemnt.classList.add(classToApply)
+        selectedChoice.parentElement.classList.add(classToApply)
 
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply)
             getNewQuestion()
+
         }, 1000)
     })
 })
 
-incrementScore = run => {}
+addScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+
+startGame()
